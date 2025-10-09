a0bee0a1e526c2b00824babd85f56483f6d36cb7
mohit-yadav2bigha <mohit@2bigha.ai>
2025-10-09 09:46:05 +0530

    metachanges

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 👈 very important for static export
+  images: {
+    unoptimized: true, // 👈 required for static export
+  },
 };
 
 export default nextConfig;
